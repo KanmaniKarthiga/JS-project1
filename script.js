5cdacfa717abbbf5d5c0b39f0b2ac4a85f3de3de
kanmanikarthiga <karthiga.selva98@gmail.com>
2023-06-09 08:30:06 +0530

    chnages

--- script.js
+++ script.js
@@ -1,56 +1,99 @@
+var hours = document.getElementById("hours")
+var minutes = document.getElementById("mins")
+var seconds = document.getElementById("secs")
+var AMPM = document.getElementById("ampm")
+var time = document.querySelectorAll(".select")
+var daytext = document.getElementById("clockmessage")
+
+
 setInterval(showTime, 1000);
 function showTime() {
     let time = new Date();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    am_pm = "AM";
+    let hrs = time.getHours();
+    let mins = time.getMinutes();
+    let secs = time.getSeconds();
+    let period = "AM"
 
-    if (hour > 12) {
-        hour -= 12;
-        am_pm = "PM";
+    if (hrs === 0) {
+        hrs = 12;
     }
-    if (hour == 0) {
-        hr = 12;
-        am_pm = "AM";
+    if (hrs >= 12) {
+        hrs -= 12;
+        period = "PM";
     }
+    console.log(`${hrs} : ${mins} : ${secs}`)
 
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
+hours.innerHTML = hrs
+minutes.innerText = mins
+seconds.innerText = secs
+AMPM.innerText = period
 
-    let currentTime = `${hour} &nbsp &nbsp &nbsp  &nbsp:  &nbsp &nbsp &nbsp &nbsp ${min}&nbsp &nbsp &nbsp &nbsp :&nbsp &nbsp &nbsp &nbsp ${sec}&nbsp &nbsp &nbsp&nbsp&nbsp &nbsp &nbsp &nbsp&nbsp&nbsp &nbsp${am_pm}`
-    document.getElementById("clock")
-        .innerHTML = currentTime;
+let mor_message = "Grab some healthy breakfast!!!"
+let noon_message = "Let's have some lunch!"
+let eve_message = "Stop Yawning,Get some tea. It's jus evening!"
+let nyt_message = "Close your eyes and Go to sleep"
+
+if (period === "AM" && hrs>=1 && hrs<=12){
+    document.getElementById("clockmessage").innerHTML = mor_message
+}
+else if(period === "PM" && (hrs===12 || (hrs>=1 &&hrs<=3))){
+    document.getElementById("clockmessage").innerHTML = noon_message
+}
+else if(period === "PM" && hrs >=4 && hrs<=7){
+    document.getElementById("clockmessage").innerHTML = eve_message
+}
+else{
+    document.getElementById("clockmessage").innerHTML = nyt_message
+}
+
 }
 showTime();
 
 
 
-let morningtime = document.getElementById("wakeuptime")
-let eveningtime = document.getElementById("naptime")
-let para = document.querySelector('#message')
-let images = document.querySelector('img')
-function checkTime() {
-    let time = new Date();
-    let hrs = time.getHours();
-    console.log(hrs)
-    console.log(morningtime.value);
+var morningtime = document.getElementById("wakeuptime")
+var lunchtime = document.getElementById("lunchtime")
+var eveningtime = document.getElementById("naptime")
+var nighttime = document.getElementById("nighttime")
 
-    if (parseInt(morningtime.value) === hrs) {
-        para.innerHTML = "Good Morning! Wake up!!"
-    }
-    else if (parseInt(eveningtime.value) === hrs) {
-        para.innerHTML = "Good evening!"
-        images.src = "./nap_time.svg"
-    }
-    else
-        para.innerHTML = "Invalid Input"
+
+var lefttext = document.getElementById("leftmessage")
+function setalarm() {
+
+    let mor_text = "Good Morning!! Wake Up!"
+    let noon_text = "Good Afternoon! Take some sleep"
+    let eve_text = "Good Evening!!!"
+    let nyt_text = "Good night!!!"
+    
+        let newtime = new Date();
+        let hrs = newtime.getHours();
+    
+        if (parseInt(morningtime.value) === hrs) {
+            lefttext.innerHTML = mor_text
+            img.src="./Component 30 - 1.svg"
+        }
+        if (parseInt(lunchtime.value) === hrs) {
+            lefttext.innerHTML = noon_text
+            img.src = "./lunch_time.svg"
+        }
+        if (parseInt(eveningtime.value) === hrs) {
+            lefttext.innerHTML = eve_text
+            img.src = "./nap_time.svg"
+        }
+        if (parseInt(nighttime.value) === hrs) {
+            lefttext.innerHTML = nyt_text
+            img.src = "./night time.png"
+        }
+
+        
 }
-checkTime()
+setalarm()
 
 
 let alarm = document.getElementById("wakeuptime")
+
+
+
 
 
 
